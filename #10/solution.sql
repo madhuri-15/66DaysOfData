6e469e803ca4b3d727cfb6da0083ba2fcd74a058
Madhuri Patil <60868865+madhuri-15@users.noreply.github.com>
2023-04-23 09:18:59 +0530

    Create solution.sql

---
+++ #10/solution.sql
@@ -0,0 +1,12 @@
+-- PostgreSQL
+-- Find the top 10 ranked songs in 2010.Output the rank, group name, and song name but do not show the same song twice.
+-- Sort the result based on the year_rank in ascending order.
+SELECT DISTINCT(year_rank) as rank,
+       group_name,
+       song_name
+FROM billboard_top_100_year_end
+WHERE 
+    year =  2010
+ORDER BY 
+    year_rank
+LIMIT 10;
